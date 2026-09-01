6386d5ecd84fc04978a63ca85a6a157bcf2e1bf3
MichalPC <michal.popieko@tm1.edu.pl>
2026-09-02 00:41:42 +0200

    Initial commit

---
+++ src/main/java/com/example/podzialobowiazkow/PodzialObowiazkowApplication.java
@@ -0,0 +1,13 @@
+package com.example.podzialobowiazkow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodzialObowiazkowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodzialObowiazkowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/podzialobowiazkow/PodzialObowiazkowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.podzialobowiazkow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodzialObowiazkowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
